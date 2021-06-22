eefac91f388a1c3aca328e00fa6eeffb2f66f22a
Rayaa <rghirri@gmail.com>
2021-06-22 17:19:04 +0100

    Header and sass setup complete

--- index.php
+++ index.php
@@ -1,56 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="/vendor/bootstrap-5.0.1/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="/sass/css/styles.css" />
-    <title>Green Garden Blog Home</title>
-  </head>
-  <body>
-    <!-- Header Start  -->
-    <header>
-      <!-- Navbar Links Start  -->
-      <nav class="navbar navbar-expand-md navbar-light bg-transparent">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#"><img id="nav-logo" src="uploads/Green-Garden-Logo-130x130.png" alt="Green Garden Blog Logo" /></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav text-uppercase">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">contact</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">login</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <!-- Navbar Links End  -->
-      <!-- Hero Banner Start  -->
-      <div class="hero-image container">
-        <div class="hero-text">
-          <h1>The Green Garden Blog</h1>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="/vendor/bootstrap-5.0.1/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="/vendor/normalize.css" />
+  <link rel="stylesheet" href="/css/styles.css" />
+  <title>Green Garden Blog Home</title>
+</head>
+
+<body>
+  <!-- Header Start  -->
+  <header>
+    <!-- Navbar Links Start  -->
+    <nav class="navbar navbar-expand-md navbar-light bg-transparent">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img id="nav-logo" src="uploads/Green-Garden-Logo-130x130.png"
+            alt="Green Garden Blog Logo" /></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav text-uppercase">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">login</a>
+            </li>
+          </ul>
         </div>
       </div>
-      <!-- Hero Banner End  -->
-    </header>
-    <!-- Header End  -->
-    <!-- Footer Start -->
-    <footer>
-      <!-- footer nav-links begin -->
+    </nav>
+    <!-- Navbar Links End  -->
+    <!-- Hero Banner Start  -->
+    <div class="hero-banner container-lg">
+      <picture class="hero-banner__overlay">
+        <img class="hero-banner__overlay-image" src="/uploads/Green-Garden-hero-banner-1295x264-min.png" alt="" />
+      </picture>
 
-      <!-- footer nav-links End -->
-    </footer>
-    <!-- Footer End -->
-  </body>
-  <script src="/vendor/jquery-3.6.0.min.js"></script>
-  <script src="/vendor/bootstrap-5.0.1/js/bootstrap.min.js"></script>
+      <div class="hero-banner__title">
+        <h1>The Green Garden Blog</h1>
+      </div>
+    </div>
+
+    <!-- Hero Banner End  -->
+  </header>
+  <!-- Header End  -->
+  <!-- Footer Start -->
+  <footer>
+    <!-- footer nav-links begin -->
+
+    <!-- footer nav-links End -->
+  </footer>
+  <!-- Footer End -->
+</body>
+<script src="/vendor/jquery-3.6.0.min.js"></script>
+<script src="/vendor/bootstrap-5.0.1/js/bootstrap.min.js"></script>
+
 </html>
