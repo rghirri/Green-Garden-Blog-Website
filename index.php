5fd3b6304129c96c7256ed761604ee9b49b64f7c
Rayaa <rghirri@gmail.com>
2021-06-22 10:52:42 +0100

    Changed file to php as started to work on mamp server to connect database.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="/vendor/bootstrap-5.0.1/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="/sass/css/styles.css" />
     <title>Green Garden Blog Home</title>
   </head>
   <body>
