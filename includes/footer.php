4e268af8cffce3ec23ffdfc82bf71ae337946296
Rayaa <rghirri@gmail.com>
2021-06-22 21:51:17 +0100

    Added header and footer to includes folder.

---
+++ includes/footer.php
@@ -0,0 +1,26 @@
+<!-- Footer Start -->
+<footer>
+  <!-- footer nav-links begin -->
+  <div class="footer-nav container pt-5">
+    <a href="#"><img class="footer-nav__logo" src="uploads/Green-Garden-logo-100x100.png"
+        alt="Green Garden Blog Logo" /></a>
+    <ul class="footer-nav__lists">
+      <li class="footer-nav__list">
+        <a class="footer-nav__link" aria-current="page" href="#">home</a>
+      </li>
+      <li class="footer-nav__list">
+        <a class="footer-nav__link" href="#">contact</a>
+      </li>
+      <li class="footer-nav__list">
+        <a class="footer-nav__link" href="#">login</a>
+      </li>
+    </ul>
+    <p class="footer-nav__copy">Copyright &copy; 2021 Green Garden Blog. All rights reserved</p>
+  </div>
+</footer>
+<!-- Footer End -->
+</body>
+<script src="/vendor/jquery-3.6.0.min.js"></script>
+<script src="/vendor/bootstrap-5.0.1/js/bootstrap.min.js"></script>
+
+</html>
--- index.php
+++ index.php
@@ -1,81 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require 'includes/header.php'; ?>
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="/vendor/bootstrap-5.0.1/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="/vendor/normalize.css" />
-  <link rel="stylesheet" href="/css/styles.css" />
-  <title>Green Garden Blog Home</title>
-</head>
+<!-- Post list begins -->
 
-<body>
-  <!-- Header Start  -->
-  <header>
-    <!-- Navbar Links Start  -->
-    <nav class="navbar navbar-expand-md navbar-light bg-transparent">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img id="nav-logo" src="uploads/Green-Garden-Logo-130x130.png"
-            alt="Green Garden Blog Logo" /></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav text-uppercase">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">contact</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">login</a>
-            </li>
-          </ul>
+<section class="container px-0">
+  <div class="row align-items-center content">
+    <div class="col-md-6 text-center">
+      <div class="row justify-content-center">
+        <div class="col-10 col-lg-8 blurb mb-5 mb-md-0">
+          <h2>MUSEUM OF CANDY</h2>
+          <img src="imgs/lolli_icon.png" alt="" class="d-none d-lg-inline" />
+          <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iste molestiae beatae, maiores
+            deserunt in voluptatibus aspernatur architecto excepturi delectus soluta? Ipsa, deleniti dolorem hic
+            consequatur repellat eveniet quidem voluptate necessitatibus dolorum delectus minus vitae, ut, veritatis
+            sint ipsum magnam autem nam ex deserunt debitis eaque ratione! Nobis, quidem assumenda.</p>
         </div>
       </div>
-    </nav>
-    <!-- Navbar Links End  -->
-    <!-- Hero Banner Start  -->
-    <div class="hero-banner container-lg">
-      <picture class="hero-banner__overlay">
-        <img class="hero-banner__overlay-image" src="/uploads/Green-Garden-hero-banner-1295x264-min.png" alt="" />
-      </picture>
-
-      <div class="hero-banner__title">
-        <h1>The Green Garden Blog</h1>
+    </div>
+    <div class="col-md-6">
+      <img src="imgs/gumball.png" alt="" class="img-fluid" />
+    </div>
+  </div>
+  <div class="row align-items-center content">
+    <div class="col-md-6 order-2 order-md-1">
+      <img src="imgs/sprinkles.png" alt="" class="img-fluid" />
+    </div>
+    <div class="col-md-6 text-center order-1 order-md-2">
+      <div class="row justify-content-center">
+        <div class="col-10 col-lg-8 blurb mb-5 mb-md-0">
+          <h2>MUSEUM OF CANDY</h2>
+          <img src="imgs/lolli_icon.png" alt="" class="d-none d-lg-inline" />
+          <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iste molestiae beatae, maiores
+            deserunt in voluptatibus aspernatur architecto excepturi delectus soluta? Ipsa, deleniti dolorem hic
+            consequatur repellat eveniet quidem voluptate necessitatibus dolorum delectus minus vitae, ut, veritatis
+            sint ipsum magnam autem nam ex deserunt debitis eaque ratione! Nobis, quidem assumenda.</p>
+        </div>
       </div>
     </div>
+  </div>
+</section>
 
-    <!-- Hero Banner End  -->
-  </header>
-  <!-- Header End  -->
-  <!-- Footer Start -->
-  <footer>
-    <!-- footer nav-links begin -->
-    <div class="footer-nav container pt-5">
-      <a href="#"><img class="footer-nav__logo" src="uploads/Green-Garden-logo-100x100.png"
-          alt="Green Garden Blog Logo" /></a>
-      <ul class="footer-nav__lists">
-        <li class="footer-nav__list">
-          <a class="footer-nav__link" aria-current="page" href="#">home</a>
-        </li>
-        <li class="footer-nav__list">
-          <a class="footer-nav__link" href="#">contact</a>
-        </li>
-        <li class="footer-nav__list">
-          <a class="footer-nav__link" href="#">login</a>
-        </li>
-      </ul>
-      <p class="footer-nav__copy">Copyright &copy; 2021 Green Garden Blog. All rights reserved</p>
-    </div>
-  </footer>
-  <!-- Footer End -->
-</body>
-<script src="/vendor/jquery-3.6.0.min.js"></script>
-<script src="/vendor/bootstrap-5.0.1/js/bootstrap.min.js"></script>
+<!-- Post list Ends -->
 
-</html>
+<?php require 'includes/footer.php'; ?>
