04cc2d67fd8f111b30c8ba618ab1b01eec6d865f
Rayaa <rghirri@gmail.com>
2021-06-23 03:47:30 +0100

    connected to database

--- index.php
+++ index.php
@@ -1,20 +1,52 @@
+<?php 
+$db_host = "localhost";
+$db_name = "green_garden_blog_db";
+$db_user ="ggb_cms";
+$db_pass ="rjQ2BtgiCz246XDe";
+
+$conn = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+$sql = "select *
+        FROM article
+        ORDER BY published_at;";
+$result = mysqli_query($conn,$sql);   
+
+if ($result === false){
+  echo mysqli_error($conn);
+}else{
+  $articles = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+}
+
+?>
+
 <?php require 'includes/header.php'; ?>
 
 <!-- Post list begins -->
 
+<?php if (empty($articles)) : ?>
+<p>No articles found.</p>
+<?php else : ?>
+
 <section class="wrapper  wrapper--medium">
+  <?php foreach ($articles as $article) : ?>
+  <?php if(!($article['id'] % 2 == 0 )) : ?>
   <div class="row pt-5">
     <div class="col-md-6">
       <div class="row justify-content-center">
         <div class="col-10 col-lg-9">
           <div class="post-list d-flex flex-column">
-            <h2 class="post-list__title">How to grow herbs at home</h2>
+            <h2 class="post-list__title"><?= $article['title'] ?></h2>
             <p id="meta-data">22 feb 2021 | cooking</p>
-            <p class="">Lorem ipsum dolor sit amet ac finibus gravida rutrum maximus eu est tellus sem fames sagittis
-              nulla
-              morbi leo lacinia ornare sociosqu vulputate vel donec eu facilisis ut tortor nec</p>
+            <p class=""><?= $article["content"] ?></p>
           </div>
-          <button class="btn">Grow herbs</button>
+          <button class="btn">Read More</button>
         </div>
       </div>
 
@@ -23,7 +55,7 @@
       <img class="post-list__image img-fluid" src="/uploads/herbs-min.png" alt="" class="img-fluid" />
     </div>
   </div>
-
+  <?php else : ?>
   <div class="row pt-5">
     <div class="col-md-6 order-2 order-md-1">
       <img class="post-list__image img-fluid" src="/uploads/garden-tools-min.png" alt="" class="img-fluid" />
@@ -32,17 +64,18 @@
       <div class="row justify-content-center">
         <div class="col-10 col-lg-9">
           <div class="post-list d-flex flex-column">
-            <h2 class="post-list__title">Which garden tool to use</h2>
+            <h2 class="post-list__title"><?= $article['title'] ?></h2>
             <p id="meta-data">30 jun 2021 | Tools</p>
-            <p class="">Lorem ipsum dolor sit amet ac finibus gravida rutrum maximus eu est tellus sem fames sagittis
-              nulla
-              morbi leo lacinia ornare sociosqu vulputate vel donec eu facilisis ut tortor nec</p>
+            <p class=""><?= $article['content'] ?></p>
           </div>
-          <button class="btn">Garden tools</button>
+          <button class="btn">Read More</button>
         </div>
       </div>
     </div>
   </div>
+  <?php endif; ?>
+  <?php endforeach; ?>
+  <?php endif; ?>
   <!-- Post list Pagination  -->
   <nav aria-label="Page">
     <ul class="pagination pt-5">
