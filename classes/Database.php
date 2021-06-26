a2b9efa9332c158d6dbb3818f9ae3630b0d668d9
Rayaa <rghirri@gmail.com>
2021-06-26 22:53:02 +0100

    Completed CRUD as OOP.

---
+++ classes/Database.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Database
+ *
+ * A connection to the database
+ */
+class Database{
+  /**
+     * Get the database connection
+     *
+     * @return PDO object Connection to the database server
+     */
+
+    public function getConn()
+    {
+      $db_host = "localhost";
+      $db_name = "green_garden_blog_db";
+      $db_user ="ggb_cms";
+      $db_pass ="rjQ2BtgiCz246XDe";
+
+      $dsn = 'mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8';
+      
+      try{
+        $db = new PDO($dsn, $db_user, $db_pass);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db;
+        
+      }catch (PDOException $e){
+        echo $e->getMessage();
+        exit;
+      }
+      
+    }
+
+}
--- includes/article-form.php
+++ includes/article-form.php
@@ -1,10 +1,20 @@
-<?php if (! empty($errors)): ?>
+<?php if (! empty($article->errors)): ?>
 <ul>
-  <?php foreach ($errors as $error): ?>
+  <?php foreach ($article->errors as $error): ?>
   <li><?= $error ?></li>
   <?php endforeach ?>
 </ul>
 <?php endif;  ?>
+
+<?php 
+   $currentPage = $_SERVER['REQUEST_URI'];
+    if ($currentPage == "/edit-article.php?id=$article->id" ){
+      $prePage = "article.php?id=$article->id";
+      }
+      else{
+       $prePage = "/";
+    }
+ ?>
 
 <div class="row">
   <div class="col-md-10 offset-md-1">
@@ -12,21 +22,21 @@
       <div class="mb-3">
         <label for="title" class="form-label">Title</label>
         <input type="text" class="form-control" name="title" id="title" placeholder="Article title" autocomplete="off"
-          value="<?= htmlspecialchars($title) ?>">
+          value="<?= htmlspecialchars($article->title) ?>">
       </div>
       <div class="mb-3">
         <label for="content" class="form-label">Content</label>
         <textarea class="form-control" name="content" rows="4" cols="40" id="content" placeholder="Article content"
-          rows="3"><?= htmlspecialchars($content) ?></textarea>
+          rows="3"><?= htmlspecialchars($article->content) ?></textarea>
       </div>
 
       <div class="mb-3">
         <label for="published_at" class="form-label">Published Date</label>
         <input type="text" class="form-control" name="published_at" id="published_at" placeholder="Published Date"
-          autocomplete="off" value="<?= htmlspecialchars($published_at) ?>">
+          autocomplete="off" value="<?= htmlspecialchars($article->published_at) ?>">
       </div>
-
-      <button class="btn mt-3 add_article_btn">save</button>
+      <button class="btn mt-3"><a href="<?= $prePage ?>">Back to Previous</a></button>
+      <button class="btn mt-3 mx-2 add_article_btn">save</button>
 
     </form>
   </div>
